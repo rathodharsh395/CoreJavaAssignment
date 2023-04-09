393dd02b95babafba5e786c3b5fd933bed7c12da
Harish Rathod <rathodharsh395@gmail.com>
2023-04-09 20:32:59 +0530

    loop and ..

---
+++ CommandLineArgument.java
@@ -0,0 +1,8 @@
+public class CommandLineArgument{
+ public static void main(String[] arguments){
+ 
+ for(String str:arguments){
+  System.out.println(str);
+  }
+}
+}
--- Loop/Loop9.java
+++ Loop/Loop9.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class Loop9
-{
-public static void main(Sting[] args)
-{
-Scanner hr = new Scanner(System.in);
-for(int i=1,intput==q,i++)
-{
-  char input = hr.next.();
+public class Loop9 {
+  public static void main(String[] args) {
+    Scanner hr = new Scanner(System.in);
+  }
+}
+/*for(int i=1,intput==q,i++)
+  {
+    char input = hr.next. ();*/
+
    
   
      
